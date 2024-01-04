0ce481ae72ffe0e7d7a54d39da8a0f8e62c6c02e
arafaysaleem <a.rafaysaleem@gmail.com>
2024-01-05 03:57:51 +0500

    refact(Transactions): fix update logic

--- functions/src/functions/transactions/income-expense.ts
+++ functions/src/functions/transactions/income-expense.ts
@@ -1,4 +1,5 @@
 import { FieldValue, firestore } from '../../core/firebase-admin';
+import { FirestorePaths } from '../../core/firestore-paths';
 import { TransactionType } from '../../enums/transaction_type.enum';
 
 export interface IncomeExpenseTransaction {
@@ -15,23 +16,38 @@ export const handleIncomeExpenseUpdate = async (
   transactionAfter: IncomeExpenseTransaction,
   bookId: string
 ): Promise<void> => {
-  const walletId = transactionBefore.wallet_id;
-  const typeBefore = transactionBefore.type;
-  const typeAfter = transactionAfter.type;
   const amountBefore = transactionBefore.amount;
   const amountAfter = transactionAfter.amount;
 
-  const isIncome = typeAfter === TransactionType.Income ? 1 : -1;
-  const typeChanged = typeAfter !== typeBefore ? 1 : -1;
+  if (amountBefore !== amountAfter) {
+    const oldWalletId = transactionBefore.wallet_id;
+    const newWalletId = transactionAfter.wallet_id;
+    const typeBefore = transactionBefore.type;
+    const typeAfter = transactionAfter.type;
 
-  const walletRef = firestore.doc(`books/${bookId}`);
+    const beforeIsIncome = typeBefore === TransactionType.Income ? 1 : -1;
+    const afterIsIncome = typeAfter === TransactionType.Income ? 1 : -1;
+    const typeChanged = typeAfter !== typeBefore ? 1 : -1;
 
-  const newAmount = amountAfter + amountBefore * typeChanged;
-  const newBalance = FieldValue.increment(newAmount * isIncome);
+    const newAmount = amountAfter + amountBefore * typeChanged;
+    const newBalance = FieldValue.increment(newAmount * afterIsIncome);
 
-  await walletRef.update({ balance: newBalance });
+    const oldWalletRef = firestore.doc(`${FirestorePaths.BOOKS}/${bookId}/${FirestorePaths.WALLETS}/${oldWalletId}`);
 
-  console.log(`Updated balance for wallet ${walletId} to ${newBalance}.`);
+    if (oldWalletId !== newWalletId) {
+      // if the wallet has changed, update old and new wallets
+      const newWalletRef = firestore.doc(`${FirestorePaths.BOOKS}/${bookId}/${FirestorePaths.WALLETS}/${newWalletId}`);
+
+      await Promise.all([
+        oldWalletRef.update({ balance: FieldValue.increment(-amountBefore * beforeIsIncome) }),
+        newWalletRef.update({ balance: newBalance })
+      ]);
+    } else {
+      // if the wallet hasn't changed, only update the wallet
+      await oldWalletRef.update({ balance: newBalance });
+    }
+    console.log(`Updated balance for wallet ${oldWalletId} to ${newBalance}.`);
+  }
 };
 
 
