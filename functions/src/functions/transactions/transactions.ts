60b0dbf1a4d3321168365bddc78862fb9a176fa0
arafaysaleem <a.rafaysaleem@gmail.com>
2024-01-06 09:07:03 +0500

    refact(Transactions): remove income expense

--- functions/src/functions/transactions/transactions.ts
+++ functions/src/functions/transactions/transactions.ts
@@ -2,7 +2,6 @@ import { onDocumentUpdated } from 'firebase-functions/v2/firestore';
 import { FirestorePaths } from '../../core/firestore-paths';
 import { TransactionType } from './../../enums/transaction_type.enum';
 import { BalanceTransferTransaction, handleBalanceTransferUpdate } from './balance-transfer';
-import { handleIncomeExpenseUpdate, IncomeExpenseTransaction } from './income-expense';
 
 export const updateWalletBalanceOnTransactionUpdate = onDocumentUpdated(
   `${FirestorePaths.BOOKS}/{bookId}/{transactionCollectionId}/{transactionId}`,
@@ -10,13 +9,7 @@ export const updateWalletBalanceOnTransactionUpdate = onDocumentUpdated(
     const transactionBefore = event.data!.before.data();
     const transactionAfter = event.data!.after.data();
 
-    if (transactionAfter.type === TransactionType.Income || transactionAfter.type === TransactionType.Expense) {
-      await handleIncomeExpenseUpdate(
-        transactionBefore as IncomeExpenseTransaction,
-        transactionAfter as IncomeExpenseTransaction,
-        event.params.bookId
-      );
-    } else if (transactionAfter.type === TransactionType.Transfer) {
+    if (transactionAfter.type === TransactionType.Transfer) {
       await handleBalanceTransferUpdate(
         transactionBefore as BalanceTransferTransaction,
         transactionAfter as BalanceTransferTransaction,
